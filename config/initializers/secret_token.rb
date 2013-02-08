da384260c281b979d20ab004294def854c5d2953
Denise Schynol <erdnusskeks@googlemail.com>
2013-02-08 18:14:26 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PonyApp::Application.config.secret_token = 'e83070fc25b6e114047b2537ee630808d51979a894e7c2f13e853935de69378673a66055d3911f71a02d53b886f7326f04589af7b7fe247e406bfdfb24fc613c'
